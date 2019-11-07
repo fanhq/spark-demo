3e4a507115b994adf2db933d862eeef0b4e195cb
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-11-07 16:11:21 +0800

    init

--- src/main/java/com/fanhq/example/sql/SparkSqlDemo4.java
+++ src/main/java/com/fanhq/example/sql/SparkSqlDemo4.java
@@ -3,6 +3,7 @@ package com.fanhq.example.sql;
 import com.fanhq.example.common.Person;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.*;
 
 /**
@@ -22,11 +23,12 @@ public class SparkSqlDemo4 {
                 .javaRDD()
                 .map(line -> {
                     Person person = new Person();
-                    person.setAge((int)line.getLong(0));
+                    person.setAge((int) line.getLong(0));
                     person.setName(line.getString(1));
                     person.setSex(line.getString(2));
                     return person;
                 });
+        peopleRDD.foreach((VoidFunction<Person>) person -> System.out.println(person.getName()));
 
         Dataset<Row> peopleDF = spark.createDataFrame(peopleRDD, Person.class);
         peopleDF.createOrReplaceTempView("person");
