bc34252c0734b456f181731984b12ee7172c5f4f
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-11-07 10:58:10 +0800

    init

---
+++ src/main/java/com/fanhq/example/SparkApplication.java
@@ -0,0 +1,21 @@
+package com.fanhq.example;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * @author fanhaiqiu
+ * @date 2019/9/17
+ */
+public class SparkApplication {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("appName").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> lines = sc.textFile("data/data.txt");
+        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+        int totalLength = lineLengths.reduce((a, b) -> a + b);
+        System.out.println(totalLength);
+    }
+}
