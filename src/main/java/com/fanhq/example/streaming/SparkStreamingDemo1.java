b8be947b22ebd2921eb918508cbbe3174fb391b9
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-11-12 16:05:20 +0800

    init

--- src/main/java/com/fanhq/example/streaming/SparkStreamingDemo1.java
+++ src/main/java/com/fanhq/example/streaming/SparkStreamingDemo1.java
@@ -1,17 +1,17 @@
 package com.fanhq.example.streaming;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * @author fanhaiqiu
@@ -20,34 +20,35 @@ import java.util.Iterator;
 public class SparkStreamingDemo1 {
 
     public static void main(String[] args) throws Exception{
-        SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkStreamingDemo1");
-        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(3));
-        JavaDStream<String> dStream = jsc.textFileStream("data/in");
-        JavaDStream<String> words = dStream.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterator<String> call(String s) throws Exception {
-                System.out.println(s);
-                return Arrays.asList(s.split(" ")).iterator();
-            }
-        });
-        //计算每个单词出现的个数
-        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String s) throws Exception {
-                return new Tuple2<String, Integer>(s, 1);
-            }
-        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer integer, Integer integer2) throws Exception {
-                return integer + integer2;
-            }
-        });
-        //输出结果
-        wordCounts.print();
 
-        //开始作业
-        jsc.start();
-        jsc.awaitTermination();
-        jsc.close();
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("SparkStreamingDemo1");
+
+        // Create the context
+        JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, Durations.seconds(5));
+
+        // Create the queue through which RDDs can be pushed to
+        // a QueueInputDStream
+
+        // Create and push some RDDs into the queue
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(i);
+        }
+
+        Queue<JavaRDD<Integer>> rddQueue = new LinkedList<>();
+        for (int i = 0; i < 30; i++) {
+            rddQueue.add(ssc.sparkContext().parallelize(list));
+        }
+
+        // Create the QueueInputDStream and use it do some processing
+        JavaDStream<Integer> inputStream = ssc.queueStream(rddQueue);
+        JavaPairDStream<Integer, Integer> mappedStream = inputStream.mapToPair(
+                i -> new Tuple2<>(i % 10, 1));
+        JavaPairDStream<Integer, Integer> reducedStream = mappedStream.reduceByKey(
+                (i1, i2) -> i1 + i2);
+
+        reducedStream.print();
+        ssc.start();
+        ssc.awaitTermination();
     }
 }
